3f3b6815f0690606ab5e0b6d224a2467ad5e262b
Paula <Paula@Paulas-MacBook-Pro.local>
2022-10-14 13:55:40 +0200

    avanzado cosas

---
+++ app/routes.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from app import app
+from flask import render_template, request, url_for, redirect, session
+import json
+import os
+import sys
+
+@app.route('/')
+@app.route('/principal')
+def principal():
+    print ("hola")
+    print (url_for('static', filename='css/si1.css'), file=sys.stderr)
+    catalogue_data = open(os.path.join(app.root_path,'catalogue/inventario.json'), encoding="utf-8").read()
+    catalogue = json.loads(catalogue_data)
+    return render_template('principal.html', title = "Home")
+
+@app.route('/frame')
+def frame():
+    print ("hola")
+    print (url_for('static', filename='css/si1.css'), file=sys.stderr)
+    # print (url_for(filename='/templates/frame.html'), file=sys.stderr)
+    return render_template('frame.html', title = "Home")
+
+
+@app.route('/index')
+def index():
+    print (url_for('static', filename='css/si1.css'), file=sys.stderr)
+    catalogue_data = open(os.path.join(app.root_path,'catalogue/inventario.json'), encoding="utf-8").read()
+    catalogue = json.loads(catalogue_data)
+    return render_template('index.html', title = "Home", movies=catalogue['peliculas'])
+
+@app.route('/acceso', methods=['GET', 'POST'])
+def login():
+    # doc sobre request object en http://flask.pocoo.org/docs/1.0/api/#incoming-request-data
+    if 'username' in request.form:
+        # aqui se deberia validar con fichero .dat del usuario
+        if request.form['username'] == 'pp':
+            session['usuario'] = request.form['username']
+            session.modified=True
+            # se puede usar request.referrer para volver a la pagina desde la que se hizo login
+            return redirect(url_for('principal'))
+        else:
+            # aqui se le puede pasar como argumento un mensaje de login invalido
+            return render_template('registro.html', title = "Registro")
+    else:
+        # se puede guardar la pagina desde la que se invoca 
+        session['url_origen']=request.referrer
+        session.modified=True        
+        # print a error.log de Apache si se ejecuta bajo mod_wsgi
+        print (request.referrer, file=sys.stderr)
+        return render_template('registro.html', title = "Registro")
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('index'))
