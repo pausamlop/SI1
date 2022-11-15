0f6c953f391ffbc3ce4b8c645e42e19c16b333ad
Elena Cano <56363130+elenacano@users.noreply.github.com>
2022-11-15 18:36:04 +0100

    Apartado F funciona
    update en actualiza

--- practica2/app/sql/actualiza.sql
+++ practica2/app/sql/actualiza.sql
@@ -154,3 +154,157 @@ SELECT setCustomersBalance(100);
 -- SELECT * FROM public.imdb_actormovies
 
 -- SELECT * FROM public.imdb_actormovies_v1
+
+
+
+-----------------------------------INTEGRIDAD DE LOS DATOS----------------------------------
+
+
+
+----------1. Para imdb_moviecountries---------------
+
+--Creamos la tabla vacia 
+CREATE TABLE public.imdb_countries (
+    countryid INT GENERATED ALWAYS AS IDENTITY, 
+    country varchar (32) not null,
+    PRIMARY KEY (countryid)
+    
+);
+
+--Añadimos los paises de las peliculas a la tabla creada
+INSERT INTO public.imdb_countries (country)
+SELECT DISTINCT country 
+FROM imdb_moviecountries
+ORDER BY country;
+
+
+--Añadimos a imdb_moviecountries la nueva columna
+ALTER TABLE imdb_moviecountries
+ADD countryid INT;
+
+--Ponemos a 0 la columna (por si acaso)
+UPDATE public.imdb_moviecountries
+SET countryid = 0;
+
+
+--Poblamos la nueva columna
+UPDATE public.imdb_moviecountries
+SET countryid = A.countryid
+FROM public.imdb_countries as A 
+    INNER JOIN public.imdb_moviecountries as B on A.country = B.country
+WHERE imdb_moviecountries.country = A.country;
+
+
+--Eliminamos la column country
+ALTER TABLE public.imdb_moviecountries DROP COLUMN country;
+
+--Creamos una nueva primary key para relacionar imdb_moviecountries 
+ALTER TABLE public.imdb_moviecountries
+ADD CONSTRAINT imdb_moviecountries_pkey PRIMARY KEY (movieid, countryid);
+
+
+--Creamos una nueva foregin key para relacionar imdb_moviecountries con moviecountries
+ALTER TABLE public.imdb_moviecountries
+ADD CONSTRAINT fk_countryid FOREIGN KEY (countryid) REFERENCES public.imdb_countries (countryid);
+
+
+
+
+
+
+
+-------------2. Para imdb_moviegenres---------------
+
+--Creamos la tabla vacia 
+CREATE TABLE public.imdb_genres (
+    genreid INT GENERATED ALWAYS AS IDENTITY, 
+    genre varchar (32) not null,
+    PRIMARY KEY (genreid)
+    
+);
+
+--Añadimos los paises de las peliculas a la tabla creada
+INSERT INTO public.imdb_genres (genre)
+SELECT DISTINCT genre
+FROM imdb_moviegenres
+ORDER BY genre;
+
+
+--Añadimos a imdb_moviecountries la nueva columna
+ALTER TABLE imdb_moviegenres
+ADD genreid INT;
+
+--Ponemos a 0 la columna (por si acaso)
+UPDATE public.imdb_moviegenres
+SET genreid = 0;
+
+
+--Poblamos la nueva columna
+UPDATE public.imdb_moviegenres
+SET genreid = A.genreid
+FROM public.imdb_genres as A 
+    INNER JOIN public.imdb_moviegenres as B on A.genre = B.genre
+WHERE imdb_moviegenres.genre = A.genre;
+
+
+--Eliminamos la column country
+ALTER TABLE public.imdb_moviegenres DROP COLUMN genre;
+
+--Creamos una nueva primary key para relacionar imdb_moviecountries 
+ALTER TABLE public.imdb_moviegenres
+ADD CONSTRAINT imdb_moviegenres_pkey PRIMARY KEY (movieid, genreid);
+
+
+--Creamos una nueva foregin key para relacionar imdb_moviecountries con moviecountries
+ALTER TABLE public.imdb_moviegenres
+ADD CONSTRAINT fk_genreid FOREIGN KEY (genreid) REFERENCES public.imdb_genres (genreid);
+
+
+
+
+-------------3. Para imdb_movielanguages---------------
+
+--Creamos la tabla vacia 
+CREATE TABLE public.imdb_languages (
+    languageid INT GENERATED ALWAYS AS IDENTITY, 
+    language varchar (32) not null,
+    PRIMARY KEY (languageid)
+    
+);
+
+--Añadimos los paises de las peliculas a la tabla creada
+INSERT INTO public.imdb_languages (language)
+SELECT DISTINCT language
+FROM imdb_movielanguages
+ORDER BY language;
+
+
+--Añadimos a imdb_moviecountries la nueva columna
+ALTER TABLE imdb_movielanguages
+ADD languageid INT;
+
+--Ponemos a 0 la columna (por si acaso)
+UPDATE public.imdb_movielanguages
+SET languageid = 0;
+
+
+--Poblamos la nueva columna
+UPDATE public.imdb_movielanguages
+SET languageid = A.languageid
+FROM public.imdb_languages as A 
+    INNER JOIN public.imdb_movielanguages as B on A.language = B.language
+WHERE imdb_movielanguages.language = A.language;
+
+
+--Eliminamos la column country
+ALTER TABLE public.imdb_movielanguages DROP COLUMN language;
+
+--Creamos una nueva primary key para relacionar imdb_moviecountries 
+ALTER TABLE public.imdb_movielanguages
+ADD CONSTRAINT imdb_movielanguages_pkey PRIMARY KEY (movieid, languageid);
+
+
+--Creamos una nueva foregin key para relacionar imdb_moviecountries con moviecountries
+ALTER TABLE public.imdb_movielanguages
+ADD CONSTRAINT fk_languageid FOREIGN KEY (languageid) REFERENCES public.imdb_languages (languageid);
+
